fa2683213150397cb597291d79ff4e3b6c4b4680
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-09-01 18:13:06 -0300

    Adicionando pages, rotas e algumas dependências

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
+import Details from '~/pages/Details';
+import New from '~/pages/New';
+import Edit from '~/pages/Edit';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/details" component={Details} isPrivate />
+      <Route path="/new" component={New} isPrivate />
+      <Route path="/edit/:id" component={Edit} isPrivate />
+
+      <Route path="/" component={() => <h1>Error 404</h1>} />
+    </Switch>
+  );
+}
